df84664fc1ea7dbf41b50e784b21e107dd467992
rajveer0104 <163584756+rajveer0104@users.noreply.github.com>
2026-07-14 22:25:50 +0530

    settings.py updated

--- aianalyzer/aianalyzer/settings.py
+++ aianalyzer/aianalyzer/settings.py
@@ -97,7 +97,9 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://ai-video-analyser.onrender.com",
+]
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
